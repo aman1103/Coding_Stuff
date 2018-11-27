e1ea16ad651ebc9edc0fec03218cfd20907fd3de
aman1103 <45396686+aman1103@users.noreply.github.com>
2018-11-27 21:04:56 +0530

    Create Default_template.cpp

---
+++ Default_template.cpp
@@ -0,0 +1,20 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define int long long
+
+typedef vector<int> vi;
+#define PB push_back
+#define REP(i,a,b) for(i=a; i<b; i++)
+
+const int N=1e5+5;
+ 
+int32_t main()
+{
+	IOS;
+ 	 
+	return 0;
+}
