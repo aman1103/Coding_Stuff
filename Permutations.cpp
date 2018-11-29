1be11cf40e1783e14f7fee4312003371bfd25745
aman1103 <45396686+aman1103@users.noreply.github.com>
2018-11-29 12:44:09 +0530

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -17,3 +17,23 @@ int main()
 	while(next_permutation(arr,arr+n)); // shows next permutation lexicographically
 	return 0;
 }
+
+// OR
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{ int n,x;
+cin>>n;
+    vector<int> permutation;
+for (int i = 0; i < n; i++) {
+    cin>>x;
+permutation.push_back(x);
+}
+sort(permutation.begin(),permutation.end());
+do {
+    for(int j=0;j<n;j++)
+    cout<<permutation[j]<<" ";
+    cout<<endl;
+} while (next_permutation(permutation.begin(),permutation.end()));
+}
