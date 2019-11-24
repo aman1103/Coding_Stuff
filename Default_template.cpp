a0e8c5627c2949b3a3bf0753b8301b5f06563e78
aman1103 <45396686+aman1103@users.noreply.github.com>
2019-11-24 21:48:47 +0530

    Update Default_template.cpp

--- Default_template.cpp
+++ Default_template.cpp
@@ -5,12 +5,14 @@ using namespace std;
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
 #define ll long long
+#define watch(x) cout << (#x) << " is " << (x) << endl
 
 typedef vector<int> vi;
 typedef vector< vi > vvi;
 typedef pair< int,int > ii;
 typedef pair< double,double> dd;
 #define pb push_back
+#define eb emplace_back
 #define sz(a) int((a).size))
 #define all(c) c.begin(),c.end()
 #define FOR(i,a,b) for(i=a; i<b; i++)
