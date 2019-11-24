a2d8fc82a3d755ca7969fa9daed0755f1256686d
aman1103 <45396686+aman1103@users.noreply.github.com>
2019-11-24 15:27:03 +0530

    Update little_tricks.cpp

--- little_tricks.cpp
+++ little_tricks.cpp
@@ -1,4 +1,4 @@
 //To increase the precision of the output
 float x=9.9999;
-cout.precision(no of precision required);
+cout.precision(n);//n is the number of places of precision required
 cout<<x<<endl;
