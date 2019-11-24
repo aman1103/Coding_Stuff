d74b7f15de98a1ab63d87b8306c2c905c8364d1f
aman1103 <45396686+aman1103@users.noreply.github.com>
2019-11-24 22:03:15 +0530

    Update little_tricks.cpp

--- little_tricks.cpp
+++ little_tricks.cpp
@@ -11,3 +11,32 @@ cout<<n;
 
 //The output will be 1000000000000000000
 // Not 1e18
+
+
+//Find the number of digits of a number without loop
+
+number_of_digits =floor(log10(N)) + 1;//number of digtis of the number N 
+
+//Finding if a number is odd or even
+
+if (num & 1) //It is fater for bigger numbers 
+   cout << "ODD"; 
+else
+   cout << "EVEN"; 
+
+//To check if a number is a power of 2
+
+bool isPowerOfTwo (int x) //Returns true if x is a power of 2 else false
+{ 
+  /* First x in the below expression is  
+    for the case when x is 0 */
+  return x && (!(x&(x-1))); 
+} 
+
+//Upper case English alphabet to lower case
+
+ch |= ' ';//ch is a upper case english alphabet
+
+//Lower case English alphabet to upper case
+
+ch &= '_’ ;//ch is a lower case english alphabet
