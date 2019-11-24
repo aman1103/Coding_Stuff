9cb0845201409763ff9dbcf96a6d1b6d82b1d55d
aman1103 <45396686+aman1103@users.noreply.github.com>
2019-11-24 15:29:30 +0530

    Update little_tricks.cpp

--- little_tricks.cpp
+++ little_tricks.cpp
@@ -2,3 +2,12 @@
 float x=9.9999;
 cout.precision(n);//n is the number of places of precision required
 cout<<x<<endl;
+
+
+// This converts the scientific notation to the regular notation
+long long int n=1e18;
+cout<<fixed;
+cout<<n;
+
+//The output will be 1000000000000000000
+// Not 1e18
