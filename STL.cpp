108cb1353c0538e3cadbc93aede3706b4f822606
aman1103 <45396686+aman1103@users.noreply.github.com>
2020-02-16 15:43:26 +0530

    Create STL.cpp

---
+++ STL.cpp
@@ -0,0 +1,9 @@
+// Iterators - Entity with which you can access data within a container with certain restrictions.
+
+
+// String and its functions.
+
+// 1) append() - inserts additonal character at the end of the string. Can also use '+' or '+=' operator.
+string s = "Hell";
+s.append('o');
+//appends 'o' to hell
